b1ebd33d1524327605716672799a8fac065c7704
17-33441-1@student.aiub.edu <17-33441-1@student.aiub.edu>
2020-12-31 06:30:44 +0600

    plsql block added , data returned from dbms outputline

---
+++ models/customer.php
@@ -0,0 +1,36 @@
+<?php
+    include("conn.php");
+    $sql = "CREATE OR REPLACE FUNCTION validUser
+            RETURN varchar2 IS 
+            userStatus varchar2(20) := ''; 
+            BEGIN 
+            SELECT status into userStatus from users where username='".$_SESSION['username']."';
+                
+            RETURN userStatus; 
+            END; ";
+    //echo $sql;
+  
+    execute($sql);
+
+
+    $sql = "DECLARE 
+            c varchar2(20); 
+            BEGIN 
+                c := validUser(); 
+                dbms_output.put_line(c); 
+            END; ";
+
+        //print_r(getPlSqlData($sql));
+    $data=  getPlSqlData($sql);
+    
+    if($data[0]=='pending')
+    {
+       session_destroy();
+       echo  "<script>
+       alert('registration not approved');
+       window.location.href='index.php';
+       </script>";
+       
+    }
+   
+?>
